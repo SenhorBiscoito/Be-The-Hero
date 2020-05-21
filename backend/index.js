f6c867b9ae6be89f852e85e17abdc932edcd344a
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2020-05-21 01:50:21 -0300

    req.query

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+/*
+*  Rota / Recurso
+*/
+
+/* 
+*    Métodos HTTP:
+*   GET: Buscar uma informação do back-end
+*   POST: Criar uma informação no back-end
+*   PUT: Atualizar uma informação no back-end
+*   DELETE: Deltar uma informação no back-end
+*/
+
+/* 
+* Tipos de parâmetros:
+*
+*   Query Params: Parametros nomeados e enviados na rota após "?" (Filtros, paginação) "req.query"
+*   Route Params: Parâmetros utilizados para identificar recursos "req.params"
+*   Request Body:
+*/
+app.get("/users", (req, res) => {
+    const params = req.query;
+
+    console.log(params)
     return res.json({
         evento: "Semana OmniStack 11.0",
         aluno: "Jean Miranda"
